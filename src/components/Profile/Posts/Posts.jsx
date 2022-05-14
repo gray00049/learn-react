3a10683d1ae17bf773c9038953411546fd0ea3ea
gray00049 <gray00049@bk.ru>
2022-05-14 23:39:18 +0300

    Add post template

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,12 +1,14 @@
 import classes from './Post.module.css';
-// import SinglePost from './SinglePost'
+import SinglePost from './SinglePost'
 
 let Posts = () => {
   return (
     <div className={classes.postWrapper}>
+
       <div className={`${classes.postSidebar} block`}>
         Post`s sidebar
       </div>
+
       <div className={classes.postList}>
         <p className={classes.title}>
           Create New Post
@@ -18,7 +20,13 @@ let Posts = () => {
             Send
           </button>
         </div>
+
+        <div className={classes.postList}>
+          <SinglePost message=" Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu" />
+          <SinglePost message="Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu" />
+        </div>
       </div>
+
     </div>
   )
 }
--- src/components/Profile/Posts/SinglePost.jsx
+++ src/components/Profile/Posts/SinglePost.jsx
@@ -1,8 +1,22 @@
 import classes from './Post.module.css';
+import userLogo from '../../../img/user_logo.jpg';
 
-let SinglePost = () => {
+let SinglePost = (props) => {
   return (
-    <div></div>
+    <div className={ `${classes.post} block` }>
+      <div className={classes.userInfo}>
+        <img src={userLogo} alt="" className={classes.photo} />
+        <p className={classes.name}>
+          Rob Sobers
+          <span className={classes.username}>
+            @rsobers
+          </span>
+        </p>
+      </div>
+      <p className={classes.postText}>
+        {props.message}
+      </p>
+    </div>
   )
 }
 
